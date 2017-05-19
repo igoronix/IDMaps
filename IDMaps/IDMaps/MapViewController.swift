941b64dd56ec293c26fc734b10a58a2d8d8709ed
Igor.D <igor.dorogokuplia@INTELLECTSOFT.NET>
2017-05-19 22:00:58 +0300

    Initial commit of IDMaps

---
+++ IDMaps/IDMaps/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IDMaps
+//
+//  Created by Igor Dorofix on 5/19/17.
+//  Copyright © 2017 ID. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet var mapContainer: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
